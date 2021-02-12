46e4e7f73477d6e588eb50fbb01802ee12d6639e
fcoverdugoa <fcoverdugoa@underpost.net>
2021-02-12 00:54:22 -0300

    Fix Mailer

--- ywork/mailer.js
+++ ywork/mailer.js
@@ -1,5 +1,50 @@
 
 
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function sendEmail(obj, fn) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  // let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: data.mailer.host,
+    port: data.mailer.port,
+    secure: data.mailer.secure, // true for 465, false for other ports
+    auth: {
+      user: data.mailer.user, // generated ethereal user
+      pass: data.mailer.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    // from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    // to: "bar@example.com, baz@example.com", // list of receivers
+    // subject: "Hello ✔", // Subject line
+    // text: "Hello world?", // plain text body
+    // html: "<b>Hello world?</b>", // html body
+    from: data.mailer.user,
+    to: obj.to,
+    subject: obj.subject,
+    text: obj.text,
+    html: obj.html
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  fn();
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+// main().catch(console.error);
+
+
 app.post('/confirm_email', function (req, res) {
 
   console.log('post -> confirm_email');
@@ -11,16 +56,51 @@ app.post('/confirm_email', function (req, res) {
 
   if(req.body.email){
 
+      /* sendEmail({
 
+        to: req.body.email,
+        subject: 'test',
+        text: 'txt 1',
+        html: '<b>txt 2</b>'
+
+      }, function(){
+
+        console.log(('success mailer: <'+req.body.email+'> '));
+
+        response = true;
+        res.send(JSON.stringify(response));
+        res.end();
+
+
+      }).catch(function(err) {
+
+        console.log('error mailer: <'+req.body.email+'> ', err);
+
+        res.send(JSON.stringify(response));
+        res.end();
+
+      }); */
 
   }
 
-
   //------------------------------------------
   //------------------------------------------
-
-  res.send(JSON.stringify(response));
-  res.end();
-
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------
+//------------------------------------------
